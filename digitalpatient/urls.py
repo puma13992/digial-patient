17d7b64855776ce54109224832cf3cedc64ef65e
puma13992 <>
2023-08-31 11:51:28 +0000

    Edit settings.py & urls.py to use allauth

--- digitalpatient/urls.py
+++ digitalpatient/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('patientapp.urls'), name='patientapp_urls'),
+    path('accounts/', include('allauth.urls'),)
 ]
