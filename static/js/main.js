69ab9084421afd95045dec088d55fbca557970be
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-09-01 15:42:17 +0000

    Add messages and functionality of them

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+// Code Code from I think therefore I blog
+setTimeout(function () {
+    let messages = document.getElementById('msg');
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 2500);
