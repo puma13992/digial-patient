8a768b3df22eb4497f8de27f23384d9cecd3a794
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-09-06 14:17:34 +0000

    Add view and url path for profile.html

--- patientapp/views.py
+++ patientapp/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def home(request):
     return render(request, 'index.html')
+
+def profile(request):
+    if request.user.is_authenticated:
+        return render(request, 'profile.html')
+    else:
+        messages.error(request, 'You have to be logged in to show this page.')
+        return redirect('../accounts/login/')
