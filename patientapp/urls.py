250e724a4aa2ed32968a27eef9c87ce9909a72de
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-09-08 07:29:28 +0000

    Add urls for personal_data and edit_personal_data

--- patientapp/urls.py
+++ patientapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("profile/", views.profile, name="profile")
+    path("profile/", views.profile, name="profile"),
+    path('edit_personal_data/', views.edit_personal_data, name='edit_personal_data'),
+    path('personal_data/', views.view_personal_data, name='personal_data'),
 ]
