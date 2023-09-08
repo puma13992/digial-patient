f68b068627c0745ffbca6d329f316f408fc2f623
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-09-08 07:20:35 +0000

    Create UserProfile model

--- patientapp/models.py
+++ patientapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Personal data
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, null=True)
+    last_name = models.CharField(max_length=100, null=True)
+    birthday = models.DateField(null=True)
+    address = models.TextField(null=True)
+    want_resuscitate = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.username
