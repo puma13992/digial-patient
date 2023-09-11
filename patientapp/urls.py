2f92c1ddfcad974aa9a457f424e8c541a47bfd6a
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-09-11 06:50:40 +0000

    Add url for delete medication/diseases

--- patientapp/urls.py
+++ patientapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('personal_data/', views.view_personal_data, name='personal_data'),
     path('medidis/', views.medidis, name='medidis'),
     path('medidis/edit/<int:entry_id>/', views.edit_medidis, name='edit_medidis'),
+    path('medidis/delete/<int:entry_id>/', views.delete_medidis, name='delete_medidis'),
 ]
