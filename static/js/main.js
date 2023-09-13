2d3c75483e2856af48930fc812899efb878f84cc
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-09-13 13:10:33 +0000

    Customize JS code for multiple alerts

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,10 @@
-// Code Code from I think therefore I blog
+// Code from I think therefore I blog (easily customized for multiple alerts)
 setTimeout(function () {
-    let messages = document.getElementById('msg');
-    let alert = new bootstrap.Alert(messages);
-    alert.close();
+    let messages = document.querySelectorAll('.alert'); // Alle Elemente mit der Klasse 'alert'
+    messages.forEach(function (message) {
+        message.classList.remove('show'); // Entferne die 'show'-Klasse, um die Anzeige auszublenden
+        setTimeout(function () {
+            message.remove(); // Entferne das Element aus dem DOM
+        }, 200); // Hier kannst du die Dauer der Ausblendanimation einstellen
+    });
 }, 2500);
