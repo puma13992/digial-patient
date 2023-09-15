d5224e61c93194990c7fdc53123e6117a37c7df0
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-09-15 08:14:06 +0000

    Change edit & view personal_data view for sharing account

--- patientapp/views.py
+++ patientapp/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth import logout
 from .models import UserProfile, MediDisList, Doctor, Contact
 from .forms import PersonalDataForm, MedicationListForm, DoctorForm, ContactForm
+from django.utils.crypto import get_random_string
 
 
 def home(request):
@@ -20,17 +21,26 @@ def profile(request):
 def edit_personal_data(request):
     if request.user.is_authenticated:
         user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+        form_personal_data = PersonalDataForm(instance=user_profile)
+        public_link = user_profile.public_link  
 
         if request.method == 'POST':
             form = PersonalDataForm(request.POST, instance=user_profile)
             if form.is_valid():
+                if 'share' in form.changed_data and user_profile.share:
+                    # Generate public link
+                    public_link = f"shared_link_{get_random_string()}"
+                    user_profile.public_link = public_link
+                else:
+                    user_profile.public_link = None
+
                 form.save()
                 messages.success(request, 'Your personal data has been successfully updated.')
                 return redirect('personal_data')
         else:
             form = PersonalDataForm(instance=user_profile)
 
-        return render(request, 'edit_personal_data.html', {'form': form})
+        return render(request, 'edit_personal_data.html', {'form': form, 'public_link': public_link})
 
     else:
         messages.error(request, 'You have to be logged in to show this page.')
@@ -40,7 +50,8 @@ def edit_personal_data(request):
 def view_personal_data(request):
     if request.user.is_authenticated:
         user_profile, created = UserProfile.objects.get_or_create(user=request.user)
-        return render(request, 'personal_data.html', {'user_profile': user_profile})
+        public_link = user_profile.public_link  
+        return render(request, 'personal_data.html', {'user_profile': user_profile, 'public_link': public_link})
 
     else:
         messages.error(request, 'You have to be logged in to show this page.')
