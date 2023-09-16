5bc37a11b1e7c7bf2ddca63b2ed496fe3efe9f23
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-09-16 08:55:44 +0000

    Change the duration of flash messages

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,10 @@
 // Code from I think therefore I blog (easily customized for multiple alerts)
 setTimeout(function () {
-    let messages = document.querySelectorAll('.alert'); // Alle Elemente mit der Klasse 'alert'
+    let messages = document.querySelectorAll('.alert'); 
     messages.forEach(function (message) {
-        message.classList.remove('show'); // Entferne die 'show'-Klasse, um die Anzeige auszublenden
+        message.classList.remove('show'); 
         setTimeout(function () {
-            message.remove(); // Entferne das Element aus dem DOM
-        }, 200); // Hier kannst du die Dauer der Ausblendanimation einstellen
+            message.remove(); 
+        }, 200); 
     });
-}, 2500);
+}, 5000);
