2703ca216fe1abbbf2a346ca1e7312ce3f3a7b3c
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-09-19 13:39:16 +0000

    Add comments to forms.py & correct code after validation

--- patientapp/forms.py
+++ patientapp/forms.py
@@ -2,13 +2,21 @@ from django import forms
 from .models import UserProfile, MediDisList, Doctor, Contact
 
 
+# Form personal data
 class PersonalDataForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['first_name', 'last_name', 'birthday', 'address', 'want_resuscitate', 'share']
+        fields = [
+            'first_name', 'last_name', 'birthday',
+            'address', 'want_resuscitate', 'share'
+            ]
         widgets = {
-            'birthday': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-            'address': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'birthday': forms.DateInput(
+                attrs={'class': 'form-control', 'type': 'date'}
+                ),
+            'address': forms.Textarea(
+                attrs={'class': 'form-control', 'rows': 3}
+                ),
         }
         labels = {
             'address': 'Address & city',
@@ -17,18 +25,21 @@ class PersonalDataForm(forms.ModelForm):
         }
 
 
+# Form medication/diseases
 class MedicationListForm(forms.ModelForm):
     class Meta:
         model = MediDisList
         fields = ['medication_or_disease_name', 'instructions']
 
 
+# Form doctors
 class DoctorForm(forms.ModelForm):
     class Meta:
         model = Doctor
         fields = ['doctor_name', 'details']
 
 
+# Form contacts
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
